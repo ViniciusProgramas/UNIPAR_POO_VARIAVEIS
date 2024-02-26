7d3949b196c0f2a88e8b730d394ba0d7be2cd07c
ViniciusProgramas <160771013+ViniciusProgramas@users.noreply.github.com>
2024-02-25 21:00:27 -0300

    AT1.java

---
+++ AT1.java
@@ -0,0 +1,14 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        String nome; "Vinicius"
+        float valor = 1.2f;
+        int idade = 25;
+        Integer ano = 40;
+        double peso = 50.5;
+        boolean confirma = true;
+        char sexo = 'V';
+        long numG = 323432423434;
+        byte numP = 2334
+        short numC = 123
+    }
+}
