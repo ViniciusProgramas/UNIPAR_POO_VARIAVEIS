9ff6fc71206a0b8042ed1b282c8cbb480c5ad428
ViniciusProgramas <160771013+ViniciusProgramas@users.noreply.github.com>
2024-02-25 21:07:51 -0300

    AT4.java

---
+++ AT4.java
@@ -0,0 +1,10 @@
+public class ControleEstoque {
+    public static void main(String[] args) {
+        // Declaração das variáveis aqui!!
+        String nomeProdu;
+        String categoriaProdu
+        double precoUnitario;
+        int qtdEmProduto
+
+    }
+}
